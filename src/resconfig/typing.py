cb1b184265ba3429bdafc6224faeb7b8c8e183a7
Taro Sato <okomestudio@gmail.com>
2020-04-05 10:57:24 -0700

    Add WatchFunction type hint

--- src/resconfig/typing.py
+++ src/resconfig/typing.py
@@ -7,4 +7,11 @@ from typing import Text  # noqa
 from typing import Tuple  # noqa
 from typing import Union  # noqa
 
+from .actions import Action
+
 Key = Union[str, Tuple[str]]
+
+WatchFunction = Callable[[Action, Any, Any], None]
+"""Callback function, i.e., watcher, that triggers on an event happening at the key. It
+takes in Action value, old, and new values.
+"""
