add91c60b28aff78177660707eb003a7c612199e
Taro Sato <okomestudio@gmail.com>
2020-04-06 16:41:58 -0700

    Implement skip_load_on_init (closes #7)

--- src/resconfig/__init__.py
+++ src/resconfig/__init__.py
@@ -1,5 +1,5 @@
+from .actions import Action  # noqa
 from .resconfig import ResConfig  # noqa
-from .resconfig.actions import Action  # noqa
 
 __author__ = "Taro Sato"
 __author_email__ = "okomestudio@gmail.com"
