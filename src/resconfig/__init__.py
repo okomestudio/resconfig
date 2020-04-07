5328446b0ebadc8f3bad1ee460081700e52e9a6b
Taro Sato <okomestudio@gmail.com>
2020-04-06 19:43:55 -0700

    Version 20.4.0a

--- src/resconfig/__init__.py
+++ src/resconfig/__init__.py
@@ -3,5 +3,5 @@ from .resconfig import ResConfig  # noqa
 
 __author__ = "Taro Sato"
 __author_email__ = "okomestudio@gmail.com"
-__version__ = "20.4.0.dev0"
+__version__ = "20.4.0a"
 __license__ = "Apache-2.0"
